5ed9879661d028421cd63d7438a7c6ab723774e8
Emmanuel Asante <emmanuela.asante@gmail.com>
2012-06-23 19:22:52 -0600

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Surveyapp::Application.config.secret_token = '20e1d597ee71ff0b37fd990819f393bfae16582ee022730097c2552565a56dbe82b3af0dce626cf4095a31ade02a3d34e6ddba090151f38c5c6c79d483bedded'
