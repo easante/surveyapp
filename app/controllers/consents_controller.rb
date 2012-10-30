0d2afb540cb13245d92fa01d8d51fbf60d1843ab
Emmanuel Asante <emmanuela.asante@gmail.com>
2012-10-29 21:56:16 -0600

    Fixed acceptance url

--- app/controllers/consents_controller.rb
+++ app/controllers/consents_controller.rb
@@ -2,10 +2,11 @@ class ConsentsController < ApplicationController
   before_filter :verify_human
 
   def home
-    if params[:locale] == "en"
+    if params[:locale] == "en" || params[:locale] == nil
       render "consents/home"
     else
       render "consents/home_es"
+    #raise params[:locale].inspect
     end
   end
 
